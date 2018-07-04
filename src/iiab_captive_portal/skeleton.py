888e3c3861620323b433e75dad6fc3c41081fc1e
Anish Mangal <anishmg@umich.edu>
2018-07-04 15:41:27 +0530

    Try and inherit basehttprequesthandler gracefully

--- src/iiab_captive_portal/skeleton.py
+++ src/iiab_captive_portal/skeleton.py
@@ -37,7 +37,8 @@ config = {}
 
 class CaptivePortal(http.server.BaseHTTPRequestHandler):
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        http.server.BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
         global config
         #this is the index of the captive portal
         #it simply redirects the user to the to login page
